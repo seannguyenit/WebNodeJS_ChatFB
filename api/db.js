87e2512cc30ef9e33f851c6dec9f298e360d4b3d
Sean Nguyen <thinhspgood@gmail.com>
2021-10-16 00:22:00 +0700

    fix connect

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "webchat",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASS || "Thinh@123",
   database: process.env.DB_NAME || "simpleweb"
 });
