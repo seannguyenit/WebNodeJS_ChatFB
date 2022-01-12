435174d4b8347e4c56a913cb1cea3ebf1bf9d470
Sean Nguyen <thinhspgood@gmail.com>
2022-01-13 02:47:51 +0700

    fix js data table

--- api/controllers/AccountsController.js
+++ api/controllers/AccountsController.js
@@ -22,21 +22,21 @@ module.exports = {
         })
     },
     get_group: (req, res) => {
-        let group = req.params.group||0;
+        let group = req.params.group || 0;
         let sql = 'CALL `account_getall`(0)'
-        db.query(sql,[group], (err, response) => {
+        db.query(sql, [group], (err, response) => {
             if (err) throw err
-            let new_res = helper.acc_filter_group(response[0],group);
+            let new_res = helper.acc_filter_group(response[0], group);
             // console.log(new_res);
             res.json(new_res)
         })
     },
     detail: (req, res) => {
-        let is_admin = req.params.is_admin||false;
+        let is_admin = req.params.is_admin || false;
         let sql = 'CALL `account_getdetail`(?)'
         db.query(sql, [req.params.id], (err, response) => {
             if (err) throw err
-            var dt= response[0][0];
+            var dt = response[0][0];
             // if(is_admin){
             //     dt.pass = dt.pass;
             // }else{
@@ -46,16 +46,27 @@ module.exports = {
         })
     },
     check_exist: (req, res) => {
-        let sql = 'CALL `user_check_existed`(?,?,?,?)'
-        db.query(sql, [req.params.id,req.params.username,req.params.role_id,req.params.group_id], (err, response) => {
+        let sql_user = 'select * from `account`'
+        db.query(sql_user, (err, response) => {
             if (err) throw err
-            var dt= response[0][0];
-            // if(is_admin){
-            //     dt.pass = dt.pass;
-            // }else{
-
-            // }
-            res.json(dt);
+            var users = response;
+            let sql_group = 'select * from `group`'
+            db.query(sql_group, (err, response) => {
+                if (err) throw err
+                var groups = response;
+                var ext = users.findIndex(f => { return f.id != req.params.id && f.user == req.params.username }) > -1 ? 1 : 0;
+                var ext_role = 0;
+                var lst_gr_param = req.params.group_id.split(',');
+                lst_gr_param.forEach(it => {
+                    var gr_selected = groups.find(fg=>{return fg.id == it})
+                    var select_role = (JSON.parse(gr_selected.role_limits || '[]')).find(f => { return f.id == req.params.role_id })
+                    var count_cr_u = users.filter(ft => { return ft.id != req.params.id && ft.role == req.params.role_id && ft.group_id != null && (ft.group_id.split(',').findIndex(fig => { return fig == it }) > -1) }).length
+                    if (select_role != null && count_cr_u >= select_role.value) {
+                        ext_role = 1;
+                    }
+                });
+                res.json({ existed: ext, over_limit: ext_role })
+            })
         })
     },
     update: (req, res) => {
@@ -64,7 +75,7 @@ module.exports = {
         let sql = 'UPDATE account SET ? WHERE id = ?'
         db.query(sql, [data, accountId], (err, response) => {
             if (err) throw err
-            res.json({message: 'Update success!'})
+            res.json({ message: 'Update success!' })
         })
     },
     store: (req, res) => {
@@ -73,43 +84,43 @@ module.exports = {
         let sql = 'INSERT INTO account SET ?'
         db.query(sql, [data], (err, response) => {
             if (err) throw err
-            res.json({message: 'Insert success!'})
+            res.json({ message: 'Insert success!' })
         })
     },
     delete: (req, res) => {
         let sql = 'DELETE FROM account WHERE id = ?'
         db.query(sql, [req.params.id], (err, response) => {
             if (err) throw err
-            res.json({message: 'Delete success!'})
+            res.json({ message: 'Delete success!' })
         })
     },
-    login: (req,res)=>{
+    login: (req, res) => {
         var er = '';
         let data = req.body;
         let sql = 'CALL `login`(?,?)'
         try {
-            db.query(sql, [data.user,data.pass], (err, response) => {
+            db.query(sql, [data.user, data.pass], (err, response) => {
                 if (err) throw err
                 // console.log(response);
                 let dt = response[0][0];
-                if(dt){
-                    res.json({ok:1,data: response[0][0]});
-                }else{
-                    res.json({ok:0});
+                if (dt) {
+                    res.json({ ok: 1, data: response[0][0] });
+                } else {
+                    res.json({ ok: 0 });
                 }
             })
         } catch (error) {
             er = error;
         }
     },
-    logout: (req,res)=>{
+    logout: (req, res) => {
         let data = req.body;
         let sql = 'CALL `logout`(?,?)'
         try {
-            db.query(sql, [data.user,data.token], (err, response) => {
+            db.query(sql, [data.user, data.token], (err, response) => {
                 if (err) throw err
                 // console.log(response);
-                res.json({ok:1});
+                res.json({ ok: 1 });
             })
         } catch (error) {
             er = error;
